f56b1ddeeed6c2bfa18c955519c908cb76d76073
hoangteoNaver <51512556+hoangteo0103@users.noreply.github.com>
2024-01-07 19:39:07 +0700

    Add example for API

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -1,16 +1,20 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ApiProperty } from '@nestjs/swagger';
 import mongoose, { Document } from 'mongoose';
 
 export type UserDocument = User & Document;
 
 @Schema()
 export class User {
+  @ApiProperty({ example : "taisiu@gmail.com"})
   @Prop({ required: true, unique: true })
   email?: string;
 
+  @ApiProperty({ example : "taisiu0103"})
   @Prop({ required: false, unique: false })
   username?: string;
 
+  @ApiProperty({ example : "secretpassword"})
   @Prop({
     required: function () {
       return this.username ? true : false;
@@ -18,6 +22,7 @@ export class User {
   })
   password?: string;
 
+  @ApiProperty({ example : "Bui Nguyen Hoang"})
   @Prop({
     required: function () {
       return this.username ? true : false;
